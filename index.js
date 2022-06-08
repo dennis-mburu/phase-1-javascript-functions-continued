7a66473bb8028704739267561beb945cca43a381
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-06-08 08:54:06 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(sth="go to the office"){
+    return `This Monday, I will ${sth}.`;
+}
+
+// function wrapAdjective (){
+
+// const wrapAdjective = function (){
+//     return function inner (par="special"){
+//         return function inner2(){
+//             return ``
+//         }
+//     }
+// }
+
+const wrapAdjective = function(string="*"){
+    return function inner (par = "special"){
+        return `You are ${string}${par}${string}!`
+    }
+}
